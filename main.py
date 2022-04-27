2fe5fec6c2cd3ff0b16aac9cd1887ba0d69e9d5a
naut <jrbarnachea@gmail.com>
2022-04-26 17:11:47 -0700

    Update main.py
    Add logout route.

--- main.py
+++ main.py
@@ -106,6 +106,12 @@ def login():
             login_user(user)
             return redirect(url_for('get_all_posts'))
     return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('get_all_posts'))
     
 
 @app.route("/post/<int:post_id>")
