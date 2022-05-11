abe869c6c6d7ccf8c12e07dc920074243ad853f1
naut <jrbarnachea@gmail.com>
2022-05-11 09:13:15 -0700

    Create scripts.js

---
+++ static/js/scripts.js
@@ -0,0 +1,32 @@
+const lazyLoad = () => {
+  var myLazyLoad = new LazyLoad();
+}
+
+const blobs = () => {
+  anime({
+    targets: '.c-wip__header',
+    opacity: 1,
+    delay: 300,
+    translateY: [-90,0],
+    duration: 1000,
+    easing: 'easeInOutExpo'
+  });
+
+  anime({
+    targets: '.c-wip__blob',
+    opacity: 1,
+    translateX: [0,200],
+    translateY: [0,-200],
+    duration: 8000,
+    easing: 'easeInOutBack',
+    direction: 'alternate',
+    delay: anime.stagger(500),
+    autoplay: true,
+    loop: true
+  });
+}
+
+$(document).ready(function() {
+  lazyLoad();
+  blobs();
+});
