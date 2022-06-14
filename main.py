ff3fb54f8480731dd2d7b3e0e13b45a8530fecce
naut <jrbarnachea@gmail.com>
2022-06-14 16:11:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
+
 # Initialize
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY")
